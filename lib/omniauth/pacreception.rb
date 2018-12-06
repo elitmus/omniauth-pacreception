f84bcff9eb5176711c4ded887db50db2199c7082
AkashSrivastava <akashsrivastava@Apples-MacBook-Pro.local>
2018-12-06 13:20:50 +0530

    First Commit

---
+++ lib/omniauth/pacreception.rb
@@ -0,0 +1,9 @@
+require "omniauth/pacreception/version"
+require "omniauth/strategies/pacreception"
+
+module Omniauth
+  module Pacreception
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/omniauth/pacreception/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Pacreception
+    VERSION = "0.1.0"
+  end
+end
